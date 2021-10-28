07f3f2d8b2d591b45e48322b05cc0de16161d20d
Firdavs468 <84624389+Firdavs468@users.noreply.github.com>
2021-10-28 17:04:10 +0500

    we started

---
+++ PulBackCashBack/View/MainVC.swift
@@ -0,0 +1,16 @@
+//
+//  MainVC.swift
+//  PulBackCashBack
+//
+//  Created by Firdavs Zokirov  on 28/10/21.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "MAIN"
+    }
+}
